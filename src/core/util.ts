2eddea8ab9b1d3a2509a7db24f5f0b45b9bdbcf7
Daniel Powley <danpowley@gmail.com>
2022-01-12 21:56:43 +0000

    Original Util.ts file

--- src/core/util.ts
+++ src/core/util.ts
@@ -1,19 +1,43 @@
-export const Util = {
-  applyDeepDefaults: function (respeonseData: {}, defaults: object[], thisSet: {}) {
-    // applies the defaults given to the responseData
+export class Util {
+  public static applyDeepDefaults(data: any, defaults: any, setter: (parent: any, item: any, val: any) => void) {
+      if (Array.isArray(defaults)) {
+          for (const subItem in data) {
+              if (data.hasOwnProperty(subItem)) {
+                  Util.applyDeepDefaults(data[subItem], defaults[0], setter);
+              }
+          }
+      } else {
+          for (const item in defaults) {
+              if (typeof defaults[item] === "object") {
+                  const isArray = Array.isArray(defaults[item]);
 
-    // m.applyDeepDefaults = function(e, t, n) {
-    //   if (Array.isArray(t))
-    //       for (var r in e)
-    //           e.hasOwnProperty(r) && m.applyDeepDefaults(e[r], t[0], n);
-    //   else
-    //       for (var o in t) {
-    //           var i;
-    //           "object" == _typeof(t[o]) ? (i = Array.isArray(t[o]),
-    //           e.hasOwnProperty(o) || n(e, o, i ? [] : {}),
-    //           m.applyDeepDefaults(e[o], t[o], n)) : e.hasOwnProperty(o) || ("function" == typeof t[o] ? n(e, o, t[o](e)) : n(e, o, t[o]))
-    //       }
-    //   return e
-    // }
+                  if (!data.hasOwnProperty(item)) {
+                      setter(data, item, isArray ? [] : {});
+                  }
+
+                  Util.applyDeepDefaults(data[item], defaults[item], setter);
+              } else {
+                  if (!data.hasOwnProperty(item)) {
+                      if (typeof(defaults[item]) === "function") {
+                          setter(data, item, defaults[item](data));
+                      } else {
+                          setter(data, item, defaults[item]);
+                      }
+                  }
+              }
+          }
+      }
+      return data;
+  }
+
+  public static treatAsUTC(date: Date): any {
+      const result = new Date(date);
+      result.setMinutes(result.getMinutes() - result.getTimezoneOffset());
+      return result;
+  }
+
+  public static daysBetween(startDate: Date , endDate: Date) {
+      const millisecondsPerDay = 24 * 60 * 60 * 1000;
+      return (this.treatAsUTC(endDate) - this.treatAsUTC(startDate)) / millisecondsPerDay;
   }
 }
