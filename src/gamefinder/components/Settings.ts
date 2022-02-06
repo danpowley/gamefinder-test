c38dc9e80b37b6043391a4c6192a88d83211ee75
Daniel Powley <danpowley@gmail.com>
2022-02-06 22:12:42 +0000

    Hide coaches by name in settings

--- src/gamefinder/components/Settings.ts
+++ src/gamefinder/components/Settings.ts
@@ -8,12 +8,16 @@ import Component from 'vue-class-component';
                 <a href="#" class="closemodal" @click.prevent="close">&times;</a>
                 <div class="settingstitle">Gamefinder settings for ALL teams</div>
                 <div class="settingssection">
-                    <div class="title"><strong>Hidden teams:</strong></div>
-                    <div>No teams currently hidden.</div>
-                </div>
-                <div class="settingssection">
                     <div class="title"><strong>Hidden coaches:</strong></div>
-                    <div>No coaches currently hidden.</div>
+                    <div style="margin-bottom: 20px;">
+                        <input type="text" width="20" placeholder="Coach name" v-model="coachNameToHide"> <button @click="hideCoach">Hide Coach</button>
+                    </div>
+                    <template v-if="hiddenCoaches.length === 0">
+                        <div>No coaches currently hidden.</div>
+                    </template>
+                    <template v-else>
+                        <div v-for="coachName in hiddenCoaches" :key="coachName"><a href="#" @click="unhideCoach(coachName)">clear</a> {{ coachName }}</div>
+                    </template>
                 </div>
             </div>
         </div>
@@ -26,6 +30,22 @@ import Component from 'vue-class-component';
     }
 })
 export default class SettingsComponent extends Vue {
+    public coachNameToHide: string = '';
+    public hiddenCoaches: string[] = [];
+
+    public hideCoach() {
+        if (! this.coachNameToHide || this.hiddenCoaches.includes(this.coachNameToHide)) {
+            this.coachNameToHide = '';
+            return;
+        }
+        this.hiddenCoaches.push(this.coachNameToHide);
+        this.coachNameToHide = '';
+    }
+
+    public unhideCoach(unhideCoachName) {
+        this.hiddenCoaches = this.hiddenCoaches.filter((coachName) => coachName !== unhideCoachName);
+    }
+
     public close() {
         this.$emit('close-modal');
     }
